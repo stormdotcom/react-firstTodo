38399762905fc4a9c8e60a8fd90c6059fd504e9b
Ajmal Nasumudeen <ajmaln73@gmail.com>
2021-11-03 17:59:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react'
+import "./app.css"
+import DeleteIcon from '@material-ui/icons/DeleteForeverOutlined';
+import EditIcon from '@material-ui/icons/EditOutlined';
+import Add from '@material-ui/icons/Add';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [list, setList] = useState("");
+    const [err, setErr] =useState(false)
+    const [items, setItems]= useState([]);
+    const [edit, setEdit]= useState(false);
+    const [id, setId]= useState("");
+    let selection;
+    const itemHandle = (event) =>{
+            setList(event.target.value)
+
+    }
+    const handleSubmit= (event) =>{
+        event.preventDefault()
+        if(list!=="") {
+            setItems((prevValue)=> [...prevValue, {text:list, isCompleted: false, id: new Date().valueOf(), isEditing:false}])
+            setErr(false)
+            setList("")
+        }
+        else {
+            setErr(true)
+        }
+    }
+    const deleteItem= (id) =>{
+            setItems((prevValue)=> {
+                return prevValue.filter((each)=> each.id !== id )
+            });
+ 
+    }
+    const handleCheckBox= (id)=>{
+        setItems((prevValue)=> {
+            return prevValue.map((each)=> {
+                if( each.id===id) {
+                    each.isCompleted=true;
+                    return each;
+                }
+                else {
+                    return each;
+                }
+          
+                
+            })
+        })
+    }   
+    const handleEdit= (id)=>{
+        setEdit(true)
+        setId(id)
+        setItems((prevValue)=> {
+            return prevValue.map((each)=>{
+                if(each.id===id){
+                    each.isEditing=true;
+                   return each;
+                }
+                else return each;
+            } )
+        });
+        
+    }
+    const editSubmit=(event)=>{
+        event.preventDefault()
+        console.log(event.target)
+            setErr(false)
+            editing(id, list)
+    }
+    const editing= (id, text)=>{
+        setEdit(false)
+        items.map((each)=>{
+        if(each.id===id) {
+            each.text= text;
+            each.isEditing=false;
+            setList("")
+            return each
+            
+        }
+        else {
+            return each
+        } 
+    })
+    }
+
+    return (
+        <div className="main">
+            <div className="card">
+               <h1> {edit ?  "Editing"  : "ToDoList" }</h1>
+               <div className="top-section">
+                   {err && <small> 	😠 Error empty input </small>}
+               {items.map((each)=> {
+
+                      each.isEditing ?
+                         selection = {
+                            border:'1px solid red',
+                            animation: "animate 1s linear forwards"
+                         } :
+                            selection = {
+                                border:'none'
+                              }
+
+                       return  <div className= {each.isCompleted ? "boxCompleted": "box"} style={selection} key={each.id}> <div className="content" ><p> {each.text} </p>   </div> 
+                       <div className="action">
+                       <input type="checkbox" checked={each.isCompleted} onChange={()=> handleCheckBox(each.id)} />
+                       {
+                           !each.isCompleted && <button onClick={()=> handleEdit(each.id)} className="btn3"><EditIcon />  </button>
+                       }
+                       
+                             <button onClick={()=> deleteItem(each.id)} className="btn2"><DeleteIcon />  </button>
+                       </div>
+
+                       
+                        </div>
+                    })}
+               </div>
+                <form className="form">
+                <input type="text" name="listItem" autoComplete="off" value={list} onChange={itemHandle} placeholder="add items" />
+                {edit ? <button  type="submit" className="button-1" onClick={(e)=>editSubmit(e)} ><EditIcon /></button>
+                    :
+                <button  type="submit" className="button-1" onClick={handleSubmit} ><Add /></button>
+                    }
+                </form>
+               
+            </div>
+
+
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./headerStyles.css"
+function Header() {
+    return (
+        <div className="navbar">
+            <h3>React</h3>
+        </div>
+    )
+}
+
+export default Header
